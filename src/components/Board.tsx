7a03cfa88c897b53b15f4c0990f130586a9598a3
PuzzleEmptyM <emptympuzzle@gmail.com>
2024-09-23 18:39:28 -0500

    some changes to the board, fixed centering as well

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -3,7 +3,7 @@ import List from './List';
 
 const Board: React.FC = () => {
   return (
-    <div className="m-auto h-screen w-screen overflow-x-scroll text-center">
+    <div className="flex justify-center items-start h-screen w-full overflow-x-auto text-center">
       <div className="flex h-full space-x-4">
         <List />
         <List />
